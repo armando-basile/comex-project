a746d77c703b5ce3aec301b8414aef4c509b5414
hmandevteam <hmandevteam@45e0aa72-986e-80e4-8fa7-380e68101923>
2011-10-28 20:54:52 +0000

    * comex-qt.sln: * comex-gtk.sln: * comex-qt.csproj: * comex-gtk.csproj: * AssemblyInfo.cs: * AssemblyInfo.cs: - update release to 0.1.6.2
    * MainWindow.ui:
    * ResManager.cs:
    * MainWindow.cs:
    * AboutDialog.cs:
    * SettingsDialog.cs:
    * MainWindow.glade:
    * MainWindowClass.Designer.cs: - changed accelerator for close file
    
    * MainClass.cs: - removed unused old code

--- comex-qt/Src/Qt_Classes/SettingsDialog.cs
+++ comex-qt/Src/Qt_Classes/SettingsDialog.cs
@@ -1,8 +1,8 @@
 /********************************************************************************
 ** Form generated from reading ui file 'SettingsDialog.ui'
 **
-** Created: lun set 5 16:52:45 2011
-**      by: Qt User Interface Compiler for C# version 4.6.3
+** Created: ven ott 28 22:52:39 2011
+**      by: Qt User Interface Compiler for C# version 4.7.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
 ********************************************************************************/
