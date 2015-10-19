896f3009ac1cc186705cdeb6e888b0e7cbdd499c
Armando Basile <armando@integrazioneweb.com>
2015-10-19 09:45:44 +0200

    update release

--- comex-base/Properties/AssemblyInfo.cs
+++ comex-base/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Armando Basile")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Armando Basile (c) 2011")]
+[assembly: AssemblyCopyright("Armando Basile (c) 2011-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,11 +17,9 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.1.8.5")]
+[assembly: AssemblyVersion("0.1.9")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 
-//[assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("comex-base.snk")]
 
