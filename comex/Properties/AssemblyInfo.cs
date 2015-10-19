8455462ba2d99d828410e8016f74c449c650e958
Armando Basile <armando@integrazioneweb.com>
2015-10-19 16:32:14 +0200

    remove release info from single project file

--- comex/Properties/AssemblyInfo.cs
+++ comex/Properties/AssemblyInfo.cs
@@ -1,8 +1,4 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
-
-// Information about this assembly is defined by the following attributes. 
-// Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("comex")]
 [assembly: AssemblyDescription("console interface for comex project")]
@@ -12,16 +8,3 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("Armando Basile (c) 2011-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("0.1.7")]
-
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
