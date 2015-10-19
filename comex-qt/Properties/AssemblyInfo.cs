e9192342e4fcfced4b05d82dd0eeb80df3c611ae
Armando Basile <armando@integrazioneweb.com>
2015-10-19 09:46:38 +0200

    update release

--- comex-qt/Properties/AssemblyInfo.cs
+++ comex-qt/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Armando Basile")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Armando Basile (c) 2011")]
+[assembly: AssemblyCopyright("Armando Basile (c) 2011-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.1.6.2")]
+[assembly: AssemblyVersion("0.1.7")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
