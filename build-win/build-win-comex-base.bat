86700272242caa23b49435fdfa7e19a86a202b15
Armando Basile <armando@integrazioneweb.com>
2015-10-20 10:14:37 +0200

    fix: use solutions subfolder

--- build-win/build-win-comex-base.bat
+++ build-win/build-win-comex-base.bat
@@ -9,4 +9,4 @@ IF NOT "%1"=="" (
 )
 
 REM Clean and Build 
-msbuild /t:Rebuild /p:Configuration=%TARGET%  ../comex-base.sln
+msbuild /t:Rebuild /p:Configuration=%TARGET%  ../solutions/comex-base.sln
